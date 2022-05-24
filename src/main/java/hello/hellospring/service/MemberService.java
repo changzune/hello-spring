ab5f1332c948da3953f525262c3f94cf944dd39b
zune <changzunelee@gmail.com>
2022-05-24 22:52:16 +0900

    mvc

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,28 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public long join(Member member){
+        //같은 이름이 있는 중복 회원 X
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미존재하는 회원입니다.");
+
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
