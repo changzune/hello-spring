929f7f3a42743d5f5169a5b62dae088e110667e2
zune <changzunelee@gmail.com>
2022-05-28 00:27:07 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/hello/hellospring/service/MemberService.java

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //controller를 설정하면 스프링이 관리
 @Controller
@@ -17,6 +20,22 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String CrateForm(){
+
+        return "members/createMemberForm";
+
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 
 
